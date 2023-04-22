ee2e4c8270007b42e64d6bae369bac88bd239b7c
Paramjot Singh <psingh@catalystone.com>
2023-04-22 13:26:04 +0530

    intial commit

---
+++ src/main/java/com/paramjot/rest/upgradedpancake/UpgradedPancakeApplication.java
@@ -0,0 +1,13 @@
+package com.paramjot.rest.upgradedpancake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpgradedPancakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpgradedPancakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paramjot/rest/upgradedpancake/UpgradedPancakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paramjot.rest.upgradedpancake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpgradedPancakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
